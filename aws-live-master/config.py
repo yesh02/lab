36f43c47c30403764d78c39f9d5fdd7f4cf27fe2
Yeshwanth <112691767+yesh02@users.noreply.github.com>
2022-12-16 10:20:24 +0530

    Delete aws-live-master directory

--- aws-live-master/config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee1.cka3tc4n3juu.us-east-1.rds.amazonaws.com"
-customuser = "admin"
-custompass = "123456789"
-customdb = "employee1"
-custombucket = "addemployee70"
-customregion = "us-east-1f"
